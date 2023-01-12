d64b52c3b21e2acb5c750260db371306a0473e7b
Kushal Agrawal <kushalagrawal17@gmail.com>
2023-01-11 21:51:46 -0600

    Separate out strategies into classes

---
+++ src/kushalplayer/LauncherStrategy.java
@@ -0,0 +1,30 @@
+package kushalplayer;
+
+import battlecode.common.*;
+
+public class LauncherStrategy {
+    /**
+     * Run a single turn for a Launcher.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runLauncher(RobotController rc) throws GameActionException {
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length > 0) {
+            MapLocation toAttack = enemies[0].location;
+
+            if (rc.canAttack(toAttack)) {
+                rc.setIndicatorString("Attacking");
+                rc.attack(toAttack);
+            }
+        }
+
+        // Also try to move randomly.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+    }
+}
