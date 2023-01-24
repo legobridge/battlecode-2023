0f19522cf0c1ff0639368a35b3fd0383bef56b35
d-niels <d.niels1998@gmail.com>
2023-01-24 10:14:22 -0600

    Improved amplifier strategy

--- src/tacoplayer/AmplifierStrategy.java
+++ src/tacoplayer/AmplifierStrategy.java
@@ -2,6 +2,7 @@ package tacoplayer;
 
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
 
 import static tacoplayer.RobotPlayer.*;
 
@@ -14,12 +15,29 @@ public class AmplifierStrategy {
     static void runAmplifier(RobotController rc) throws GameActionException {
         // Update alive counter
         Comms.updateRobotCount(rc);
-        if (rc.canMove(rc.getLocation().directionTo(mapCenter))) {
-            rc.move(rc.getLocation().directionTo(mapCenter));
+
+        // MUST move away from enemies
+        RobotInfo[] enemies = rc.senseNearbyRobots(-1, theirTeam);
+        if (enemies.length > 0) {
+            moveAwayFromRobots(rc, enemies);
+            moveAwayFromRobots(rc, enemies);
+            moveAwayFromRobots(rc, enemies);
         }
-        else {
+
+        // Alternate wandering and staying grouped with friendlies
+        RobotInfo[] friendlies = rc.senseNearbyRobots(-1, ourTeam);
+        if (rc.getRoundNum() % 2 == 0 || friendlies.length == 0) {
+            Pathing.moveRandomly(rc);
+            Pathing.moveRandomly(rc);
             Pathing.moveRandomly(rc);
         }
-    }
+        else {
+            moveTowardsRobots(rc, friendlies);
+            moveTowardsRobots(rc, friendlies);
+            moveTowardsRobots(rc, friendlies);
+        }
 
+        // Sense islands
+        Comms.updateIslands(rc);
+    }
 }
