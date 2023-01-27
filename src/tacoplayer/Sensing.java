3c334c7631224a8805b895ce02c7ecb99bc7c8f7
Kushal Agrawal <kushalagrawal17@gmail.com>
2023-01-27 13:57:17 -0600

    Add basic obstacle sensing

--- src/tacoplayer/Sensing.java
+++ src/tacoplayer/Sensing.java
@@ -7,6 +7,8 @@ import static tacoplayer.RobotPlayer.*;
 public class Sensing {
 
     final static int MAX_SENSED_ROBOTS = 120;
+    final static int MAX_RADIUS_TO_SENSE_OBSTACLES = 20;
+    final static int SMALLER_RADIUS_TO_SENSE_OBSTACLES = 13;
 
     static int ourCarrierCount;
     static int ourLauncherCount;
@@ -41,8 +43,31 @@ public class Sensing {
     static int[] knownIslandIds = new int[GameConstants.MAX_NUMBER_ISLANDS];
     static IslandInfo[] knownIslands = new IslandInfo[GameConstants.MAX_NUMBER_ISLANDS + 1]; // Island with ID i is stored at i-th index
 
-    static void scanObstacles(RobotController rc) {
-        // TODO - scan for clouds and currents and add to local memory
+    static int bcsum = 0;
+    static void scanObstacles(RobotController rc) throws GameActionException {
+//        int bc1 = Clock.getBytecodesLeft();
+        int radiusSquaredToSense = MAX_RADIUS_TO_SENSE_OBSTACLES;
+        if (turnCount == 1) { // On the robot's first turn, don't scan everything
+            radiusSquaredToSense = SMALLER_RADIUS_TO_SENSE_OBSTACLES;
+        }
+        MapInfo[] mapInfos = rc.senseNearbyMapInfos(radiusSquaredToSense);
+        for (int i = mapInfos.length; --i >= 0;) {
+            MapLocation loc = mapInfos[i].getMapLocation();
+            if (map[loc.x][loc.y] == 0) {
+                if (!mapInfos[i].isPassable()) {
+                    map[loc.x][loc.y] = 1;
+                }
+                else if (mapInfos[i].hasCloud()) {
+                    map[loc.x][loc.y] = 2;
+                } else {
+                    map[loc.x][loc.y] = 3 + mapInfos[i].getCurrentDirection().ordinal();
+                }
+            }
+        }
+//        bcsum += (bc1 - Clock.getBytecodesLeft());
+//        if (rc.getType() == RobotType.LAUNCHER && rc.getID() % 17 == 0) {
+//            System.out.println(bcsum / turnCount);
+//        }
     }
 
     static void scanRobots(RobotController rc) throws GameActionException {
